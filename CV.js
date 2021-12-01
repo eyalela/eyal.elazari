829454aa53a3e07a12b80da851dbd1db76ea307c
Eyal Elazari <eyalelazari92@gmial.com>
2021-12-01 17:16:59 +0200

    Ex6

---
+++ CV.js
@@ -0,0 +1,11 @@
+function ChangeHeaderOnLoad(){
+    document.getElementById("Title").innerHTML="Welcome to my cv grid";
+}
+
+function name_to_console() {
+    console.log('Eyal Elazari');
+}
+function seeyou() {
+    document.getElementById("Buttono").innerHTML = "See You"; 
+}
+ 
